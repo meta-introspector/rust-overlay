eee7767f08f58eb56822d7e85423098eb3e6dd65
mike <h@solfunmeme.com>
2025-10-16 21:17:00 +0000

    update url

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   '';
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
   };
 
   outputs =
